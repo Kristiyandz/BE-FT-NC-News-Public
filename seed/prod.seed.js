202fc30324e6bbc31810407b6bd3e7b8ba8d223b
kristiyandzhakov@gmial.com <kristiyandzhakov@gmail.com>
2018-03-29 13:05:29 +0100

    update

--- seed/prod.seed.js
+++ seed/prod.seed.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
-const DB_URL = process.env.DB_URL || require("../config");
+const { DB_URL } = process.env.DB_URL || require("../config");
 const seedDB = require("./seed");
 
+console.log(DB_URL)
 
 mongoose
-  .connect(DB_URL.production.DB_URL)
+  .connect(DB_URL)
   .then(() => {
     console.log(`connected to ${DB_URL}`);
     return seedDB();
