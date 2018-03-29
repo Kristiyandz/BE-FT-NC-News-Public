47ecc7ab001bed8be2634707cbd2a04adaae5261
kristiyandzhakov@gmial.com <kristiyandzhakov@gmail.com>
2018-03-29 11:30:58 +0100

    update

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 const DB_URL = process.env.DB_URL || require("../config");
 const seedDB = require("./seed");
+console.log(DB_URL);
 mongoose
   .connect(DB_URL.production.DB_URL)
   .then(() => {
     console.log(`connected to ${DB_URL}`);
-    return seedDB(DB_URL);
+    return seedDB();
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
