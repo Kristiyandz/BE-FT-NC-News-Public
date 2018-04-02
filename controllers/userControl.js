a3901258e2dbb0a0be8a915f4297c489508c60de
kristiyandzhakov@gmial.com <kristiyandzhakov@gmail.com>
2018-04-02 10:57:12 +0100

    updated user controller

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,26 +1,25 @@
 const Users = require('../models/users');
 
-// function getUsers(req, res, next) {
-//   Users.find()
-//     .then((users) => {
-//       res.status(200).send({ users });
-//     })
-// }
-function getUserById(req, res, next) {
-  const user_id = req.params.user_id
-  Users.findOne({ username: user_id })
-    .then(user => {
-      res.status(200).send({ user })
+function getUsers(req, res, next) {
+  Users.find()
+    .then((users) => {
+      res.status(200).send({ users });
     })
-
 }
 
-// function getUserById(req, res, next) {
-//   let id = req.params.user_id;
-//   Users.findById(id)
-//     .then((user) => {
-//       res.status(200).send({ user })
-//     })
-// }
+function getUserByUsername(req, res, next) {
+  const user_name = req.params.username;
+  Users.find()
+    .then(user => {
+      let arrayOfUsers = [user];
+      let result = arrayOfUsers[0].map(user => {
+        if (user.username === user_name) {
+          res.status(200).send({ user });
+        }
+      });
+      res.status(400).send({ message: 'Invalud username!' });
+    })
+    .catch(next)
+}
 
-module.exports = { getUserById };
+module.exports = { getUsers, getUserByUsername };
