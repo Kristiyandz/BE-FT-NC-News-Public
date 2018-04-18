6e530b204e6507a208af1350a1c2377f06e33311
kristiyandzhakov@gmial.com <kristiyandzhakov@gmail.com>
2018-04-18 15:50:31 +0100

    added acces to the api

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const apiRouter = require('./router/apiRouter');
 const bodyParser = require('body-parser');
 const DB_URL = process.env.DB_URL || require('./config').DB_URL;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 mongoose.connect(DB_URL)
   .then(() => {
     console.log(`connected to DB ${DB_URL}`);
