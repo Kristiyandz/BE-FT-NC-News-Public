3bca97f9f4ad521a3b39514ba82b9a184641730d
kristiyandzhakov@gmial.com <kristiyandzhakov@gmail.com>
2018-04-19 12:22:39 +0100

    updated acces level

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const DB_URL = process.env.DB_URL || require('./config').DB_URL;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
