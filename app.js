03f9065a4fff1b9bbeffdd3a906f3aeaa341e174
kristiyandzhakov@gmial.com <kristiyandzhakov@gmail.com>
2018-04-20 09:13:00 +0100

    update on app.js

--- app.js
+++ app.js
@@ -6,26 +6,12 @@ const apiRouter = require('./router/apiRouter');
 const bodyParser = require('body-parser');
 const DB_URL = process.env.DB_URL || require('./config').DB_URL;
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 mongoose.connect(DB_URL)
   .then(() => {
